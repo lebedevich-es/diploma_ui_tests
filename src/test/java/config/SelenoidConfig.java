b19f3f9cd44846053e272ba82706de80787c1f04
lebedevich-es <lebedevich.es@gmail.com>
2023-07-24 20:50:58 +0300

    added fix

--- src/test/java/config/SelenoidConfig.java
+++ src/test/java/config/SelenoidConfig.java
@@ -3,7 +3,7 @@ package config;
 import org.aeonbits.owner.Config;
 
 @Config.Sources({
-        "classpath:properties/selenoid.properties"
+        "classpath:selenoid.properties"
 })
 public interface SelenoidConfig extends Config {
 
